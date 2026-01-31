5265bc6c5a6e0a59d77fe443b360e7adaa9f37cc
YlaMarino <marinoyla333@gmail.com>
2026-02-01 01:04:52 +0800

    U: header properties and etc

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,23 @@ import ContactMe from './division/contactMe'
 
 function Header() {
   return (
-    <div className='div1' style={{ backgroundColor: "#445E4F", padding: "15px" }}>
-      <div className='d1-container1' style={{ backgroundColor: "#9ac1ff27" }}>
-        <img className='d1-img' src="/images/logo-white.png" alt="logo" style={{ width: "50px" }} />
+    <div className='div1'>
+      <div className='d1-container1'>
+        <img className='d1-img' src="/images/logo-white.png" alt="logo" />
         <p className='d1-name'> YKMarino </p>
       </div>
-      HEADER
     </div>
   )
 }
 function App() {
 
   return (
-    <div style={{ backgroundColor: "#ffc6c6", height: "800px" }}>
-    <Header />
-    <AboutMe /> 
-    {/* <ProjectsDone />
-    <TechUsed /> 
-    <ContactMe />*/}
+    <div className='div-whole' style={{ backgroundColor: "#553333", height: "800px" }}>
+      <Header />
+      <AboutMe /> 
+      {/* <ProjectsDone />
+      <TechUsed /> 
+      <ContactMe />*/}
     </div>
   )
 }
--- src/division/aboutMe.jsx
+++ src/division/aboutMe.jsx
@@ -6,13 +6,15 @@ function AboutMe() {
 
       <div className='d2-container1' style={{ backgroundColor: "#ffa69a", padding: "15px" }}>
         <div className='d2-container1-1' style={{ backgroundColor: "#ff7c6b" }}>
-          Hello World! I am
-          <b> YLA KORAZON R. MARINO </b>
-          “I’m someone who loves figuring out how things 
-          work—whether that’s building a website, 
-          sketching designs, or trying out a new hobby. 
-          Coding is just one way I bring ideas to life 
-          and challenge myself to keep learning.”
+          <p>
+            Hello World! I am
+            <b> YLA KORAZON R. MARINO </b>
+            “I’m someone who loves figuring out how things 
+            work—whether that’s building a website, 
+            sketching designs, or trying out a new hobby. 
+            Coding is just one way I bring ideas to life 
+            and challenge myself to keep learning.”
+          </p>
         </div>
         <div className='d2-container1-2' style={{ backgroundColor: "#e7604e" }}>
           buttons here
